4d4c24eae88acadf68ce3b3234ff491fbb0ebf88
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-07-02 14:05:38 +0100

    Added tests for map function

--- workshop/index.js
+++ workshop/index.js
@@ -1,3 +1,11 @@
-function map(array) {
+function map(array, callback) {
+  let results = [];
+  for (let i = 0; i < array.length; i++) {
+    results.push(callback(array[i]));
+  }
+  return results;
+}
+
+function filter(array, callback) {
   return array;
 }
--- workshop/index.test.js
+++ workshop/index.test.js
@@ -5,7 +5,32 @@ test("map() should return an array with the same number of elements", () => {
   equal(result.length, 1);
 });
 
-test("map() should transform each element of the array using the fn argument", () => {
+test("map() should transform the first index of each array using the fn argument", () => {
   const result = map([1], (x) => x + 1);
   equal(result[0], 2);
+});
+
+test("map() should transform all indexes of each array using the fn argument", () => {
+  const result = map([1, 2, 3, 4], (x) => x + 1);
+  equal(result[0], 2);
+  equal(result[1], 3);
+  equal(result[2], 4);
+  equal(result[3], 5);
+});
+
+test("map() should return an array of the same length that was given", () => {
+  const result = map([1], (x) => x * 3);
+  equal(result.length, 1);
+});
+
+test("map() should return an array ", () => {
+  const result = map([1], (x) => x * 3);
+  equal(Array.isArray(result), true);
+});
+
+// testing filter()
+
+test("filter() should return an array", () => {
+  const result = filter([1, 2], (x) => x > 1);
+  equal(Array.isArray(result), true);
 });
